3a31ff77b3cc13bca34c369fecda8a859e48e1fd
liutingna <liutingna@inspur.com>
2018-06-24 23:25:30 +0800

    update：增加条件装配的配置文件

--- Chapter_03/conditional/src/main/java/com/habuma/restfun/MagicConfig.java
+++ Chapter_03/conditional/src/main/java/com/habuma/restfun/MagicConfig.java
@@ -3,8 +3,10 @@ package com.habuma.restfun;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:conf.properties")
 public class MagicConfig {
 
   @Bean
