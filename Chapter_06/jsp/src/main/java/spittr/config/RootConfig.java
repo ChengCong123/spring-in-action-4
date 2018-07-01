0b1bc7a7f16d799ee08e173c21e3ed1cc12c2417
liutingna <liutingna@inspur.com>
2018-07-01 17:45:33 +0800

    Upd: Spring Java config配置Bean移动

--- Chapter_06/jsp/src/main/java/spittr/config/RootConfig.java
+++ Chapter_06/jsp/src/main/java/spittr/config/RootConfig.java
@@ -1,35 +1,26 @@
 package spittr.config;
 
-import java.util.regex.Pattern;
-
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
-
 import spittr.config.RootConfig.WebPackage;
+
+import java.util.regex.Pattern;
 
 @Configuration
 @Import(DataConfig.class)
-@ComponentScan(basePackages={"spittr"}, 
-    excludeFilters={
-        @Filter(type=FilterType.CUSTOM, value=WebPackage.class)
-    })
+@ComponentScan(basePackages = {"spittr"},
+        excludeFilters = {
+                @Filter(type = FilterType.CUSTOM, value = WebPackage.class)
+        })
 public class RootConfig {
-  public static class WebPackage extends RegexPatternTypeFilter {
-    public WebPackage() {
-      super(Pattern.compile("spittr\\.web"));
-    }    
-  }
-
-  @Bean(name="messageSource")
-  public MessageSource messageSource() {
-    ResourceBundleMessageSource messageSource =
-            new ResourceBundleMessageSource();
-    messageSource.setBasename("messages");
-    messageSource.setDefaultEncoding("UTF-8");
-    return messageSource;
-  }
+    public static class WebPackage extends RegexPatternTypeFilter {
+        public WebPackage() {
+            super(Pattern.compile("spittr\\.web"));
+        }
+    }
 
 }
--- Chapter_06/jsp/src/main/java/spittr/web/WebConfig.java
+++ Chapter_06/jsp/src/main/java/spittr/web/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -36,12 +37,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     super.addResourceHandlers(registry);
   }
   
+/*
   @Bean
   public MessageSource messageSource() {
     ReloadableResourceBundleMessageSource messageSource = 
         new ReloadableResourceBundleMessageSource();
     messageSource.setBasename("file:///Users/habuma/messages");
     messageSource.setCacheSeconds(10);
+    return messageSource;
+  }
+*/
+  @Bean(name="messageSource")
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource =
+            new ResourceBundleMessageSource();
+    messageSource.setBasename("messages");
+    messageSource.setDefaultEncoding("UTF-8");
     return messageSource;
   }
  
